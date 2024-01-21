7b1132ab76db6302096b6c76fc0aa29eeef875a3
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-01-21 23:57:53 +0600

    responsive done for mobile and desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         'gradient-2': 'rgba(230, 166, 35, 0.10)',
         'gradient-3':'rgba(87, 118, 57, 0.10)',
         'gradient-4': 'linear-gradient(90deg, #D9D9D9 0%, rgba(255, 255, 255, 0.00) 100%)',
+        'footer-bg':'linear-gradient(270deg, rgba(255, 0, 0, 0.10) 0%, rgba(255, 137, 56, 0.10) 100%)'
       },
       colors : {
         'gradient-color': "linear-gradient(270deg, #F00 0%, #FF8938 100%)",
